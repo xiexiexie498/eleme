cf724ffb403eadcca2aaf013029293f37e6acc7d
xiehaojie <1262174689@qq.com>
2022-04-22 16:19:02 +0800

    init gshop

---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,23 @@
+/*这是个插槽组件*/
+<template>
+  <header class="header">
+          <slot name="left"></slot>
+          <span class="header_title">
+            <span class="header_title_text ellipsis">{{ title }}</span>
+          </span>
+          <slot name="right"></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderTop',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
